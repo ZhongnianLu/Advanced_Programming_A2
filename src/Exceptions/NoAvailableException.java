2495e25fc3f476b17b6f8ec3ff1746524f629b0c
rmit-s3512993-Zhongnian-Lu <31427292+rmit-s3512993-Zhongnian-Lu@users.noreply.github.com>
2018-04-26 01:39:45 +1000

    move exceptions to exceptions package

--- src/Exceptions/NoAvailableException.java
+++ src/Exceptions/NoAvailableException.java
@@ -1,4 +1,4 @@
-package Interfaces;
+package Exceptions;
 
 public class NoAvailableException extends Exception {
 
--- src/Exceptions/NotToBeCoupledException.java
+++ src/Exceptions/NotToBeCoupledException.java
@@ -1,4 +1,4 @@
-package Interfaces;
+package Exceptions;
 
 public class NotToBeCoupledException extends Exception {
 
